e5a44a32f6d8ee9bcfe96ba3ef26d441b1e70788
Kyle Wayman <kwayman@gmail.com>
2021-12-20 12:12:46 -0800

    Add unit test placeholders for bundle.go.

---
+++ bundle_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestNewBundle(t *testing.T) {
+}
+
+func TestGetPatient(t *testing.T) {
+}
+
+func TestGetDoctor(t *testing.T) {
+}
+
+func TestGetDiagnosis(t *testing.T) {
+}
+
+func TestGetAppointment(t *testing.T) {
+}
+
+func TestAddFeedback(t *testing.T) {
+}
+
+func TestSaveFeedback(t *testing.T) {
+}
