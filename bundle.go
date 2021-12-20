c0418c1ea968fca91d7185319d70036fbec48b71
Kyle Wayman <kwayman@gmail.com>
2021-12-20 10:17:48 -0800

    Move Bundle to its own file. Add feedback output file.

---
+++ bundle.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/k0kubun/pp"
+	"io/ioutil"
+)
+
+type Bundle struct {
+	ResourceType string `json: resourceType`
+	Id           string `json: id`
+	Timestamp    string `json: timestamp`
+	Entry        []struct {
+		Resource Resource `json: entry`
+	}
+}
+
+type Resource struct {
+	ResourceType string `json: resourceType`
+	Id           string `json: id`
+
+	Active bool `json: bool`
+	Actor  struct {
+		Reference string `json: reference`
+	}
+	Address []struct {
+		Use  string   `json: use`
+		Line []string `json: line`
+	}
+	Appointment struct {
+		Reference string `json: reference`
+	}
+	BirthDate string `json: birthDate`
+	Code      struct {
+		Coding []struct {
+			System string `json: system`
+			Code   string `json: code`
+			Name   string `json: name`
+		}
+	}
+	Contact []struct {
+		System string `json: system`
+		Value  string `json: value`
+		Use    string `json: use`
+	}
+	Gender string `json: gender`
+	Meta   struct {
+		LastUpdated string `json: lastUpdated`
+	}
+	Name []struct {
+		Text   string   `json: text`
+		Family string   `json: family`
+		Given  []string `json: given`
+	}
+	Period struct {
+		Start string `json: start`
+		End   string `json: end`
+	}
+	Response map[string]string
+	Status   string `json: status`
+	Subject  struct {
+		Reference string `json: reference`
+	}
+	Type []struct {
+		Text string `json: text`
+	}
+}
+
+type Patient struct {
+	ResourceType string
+	Id           string
+	Active       bool
+	Name         []struct {
+		Text   string
+		Family string
+		Given  []string
+	}
+	Contact []struct {
+		System string
+		Value  string
+		Use    string
+	}
+	Gender    string
+	BirthDate string
+	Address   []struct {
+		Use  string
+		Line []string
+	}
+}
+
+type Feedback struct {
+	ResourceType string
+	Id           string
+	Response     map[string]string
+}
+
+func NewBundle(path string) Bundle {
+	var bundle Bundle
+
+	fmt.Println("Reading bundle...")
+	source, ferr := ioutil.ReadFile(path)
+	if ferr != nil {
+		panic(ferr)
+	}
+	jerr := json.Unmarshal(source, &bundle)
+	if jerr != nil {
+		panic(jerr)
+	}
+
+	return bundle
+}
+
+func GetPatient(bundle Bundle) Resource {
+	var resource Resource
+	for i, _ := range bundle.Entry {
+		if bundle.Entry[i].Resource.ResourceType == "Patient" {
+			resource = bundle.Entry[i].Resource
+		}
+	}
+	return resource
+}
+
+func GetDoctor(bundle Bundle) Resource {
+	var resource Resource
+	for i, _ := range bundle.Entry {
+		if bundle.Entry[i].Resource.ResourceType == "Doctor" {
+			resource = bundle.Entry[i].Resource
+		}
+	}
+	return resource
+}
+
+func GetDiagnosis(bundle Bundle) Resource {
+	var resource Resource
+	for i, _ := range bundle.Entry {
+		if bundle.Entry[i].Resource.ResourceType == "Diagnosis" {
+			resource = bundle.Entry[i].Resource
+		}
+	}
+	return resource
+}
+
+func GetAppointment(bundle Bundle) Resource {
+	var resource Resource
+	for i, _ := range bundle.Entry {
+		if bundle.Entry[i].Resource.ResourceType == "Appointment" {
+			resource = bundle.Entry[i].Resource
+		}
+	}
+	return resource
+}
+
+func AddFeedback(bundle Bundle, f map[string]string) {
+	var feedback Resource
+	feedback.ResourceType = "Feedback"
+	feedback.Id = uuid.NewString()
+	feedback.Response = make(map[string]string)
+	for key, value := range f {
+		feedback.Response[key] = value
+	}
+	// TODO: Figure out how to append onto the Entry slice
+	//bundle.Entry = append(bundle.Entry, feedback)
+	//bundle.Entry[99] = feedback
+	pp.Println(bundle)
+}
+
+func SaveFeedback(f map[string]string) {
+	var feedback Feedback
+	feedback.ResourceType = "Feedback"
+	feedback.Id = uuid.NewString()
+	feedback.Response = make(map[string]string)
+	for key, value := range f {
+		feedback.Response[key] = value
+	}
+	pp.Println(feedback)
+	jsonString, _ := json.Marshal(feedback)
+	path := fmt.Sprintf("feedback-%s.json", feedback.Id)
+	ioutil.WriteFile(path, jsonString, 0600)
+}
--- feedback.go
+++ feedback.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"flag"
 	"fmt"
-	//"github.com/k0kubun/pp"
-	"io/ioutil"
 	"os"
 	"regexp"
 	"strconv"
@@ -14,128 +11,16 @@ import (
 	"time"
 )
 
-type Bundle struct {
-	ResourceType string `json: resourceType`
-	Id           string `json: id`
-	Timestamp    string `json: timestamp`
-	Entry        []struct {
-		Resource Resource `json: entry`
-	}
-}
-
-type Resource struct {
-	ResourceType string `json: resourceType`
-	Id           string `json: id`
-
-	Active bool `json: bool`
-	Actor  struct {
-		Reference string `json: reference`
-	}
-	Address []struct {
-		Use  string   `json: use`
-		Line []string `json: line`
-	}
-	Appointment struct {
-		Reference string `json: reference`
-	}
-	BirthDate string `json: birthDate`
-	Code      struct {
-		Coding []struct {
-			System string `json: system`
-			Code   string `json: code`
-			Name   string `json: name`
-		}
-	}
-	Contact []struct {
-		System string `json: system`
-		Value  string `json: value`
-		Use    string `json: use`
-	}
-	Gender string `json: gender`
-	Meta   struct {
-		LastUpdated string `json: lastUpdated`
-	}
-	Name []struct {
-		Text   string   `json: text`
-		Family string   `json: family`
-		Given  []string `json: given`
-	}
-	Period struct {
-		Start string `json: start`
-		End   string `json: end`
-	}
-	Status  string `json: status`
-	Subject struct {
-		Reference string `json: reference`
-	}
-	Type []struct {
-		Text string `json: text`
-	}
-}
-
-type Patient struct {
-	ResourceType string
-	Id           string
-	Active       bool
-	Name         []struct {
-		Text   string
-		Family string
-		Given  []string
-	}
-	Contact []struct {
-		System string
-		Value  string
-		Use    string
-	}
-	Gender    string
-	BirthDate string
-	Address   []struct {
-		Use  string
-		Line []string
-	}
-}
-
-func NewBundle(path string) Bundle {
-	var bundle Bundle
-
-	fmt.Println("Reading bundle...")
-	source, ferr := ioutil.ReadFile(path)
-	if ferr != nil {
-		panic(ferr)
-	}
-	jerr := json.Unmarshal(source, &bundle)
-	if jerr != nil {
-		panic(jerr)
-	}
-
-	return bundle
-}
-
 func main() {
 
 	inputPath := flag.String("i", "input.txt", "input file")
 	flag.Parse()
 
-	var patient Resource
-	var doctor Resource
-	var appointment Resource
-	var diagnosis Resource
-
 	bundle := NewBundle(*inputPath)
-	for i, _ := range bundle.Entry {
-		if bundle.Entry[i].Resource.ResourceType == "Patient" {
-			patient = bundle.Entry[i].Resource
-		}
-		if bundle.Entry[i].Resource.ResourceType == "Doctor" {
-			doctor = bundle.Entry[i].Resource
-		}
-		if bundle.Entry[i].Resource.ResourceType == "Appointment" {
-			appointment = bundle.Entry[i].Resource
-		}
-		if bundle.Entry[i].Resource.ResourceType == "Diagnosis" {
-			diagnosis = bundle.Entry[i].Resource
-		}
-	}
+	patient := GetPatient(bundle)
+	doctor := GetDoctor(bundle)
+	appointment := GetAppointment(bundle)
+	diagnosis := GetDiagnosis(bundle)
 
 	reader := bufio.NewReader(os.Stdin)
 
@@ -144,13 +29,14 @@ func main() {
 
 	fmt.Printf("\nHi %s, on a scale of 1-10, would you recommend Dr %s to a friend or family member? 1 = Would not recommend, 10 = Would strongly recommend\n", patient.Name[0].Given[0], doctor.Name[0].Family)
 
-	rating := 0
-	for rating == 0 {
+	rating := ""
+	for rating == "" {
 		fmt.Print("> ")
 		ratingInput, _ := reader.ReadString('\n')
-		ratingInt, _ := strconv.Atoi(strings.TrimSuffix(ratingInput, "\n"))
+		ratingInput = strings.TrimSuffix(ratingInput, "\n")
+		ratingInt, _ := strconv.Atoi(ratingInput)
 		if 0 < ratingInt && ratingInt < 11 {
-			rating = ratingInt
+			rating = ratingInput
 		} else {
 			fmt.Println("Please, only respond with a number between 1 and 10.")
 		}
@@ -158,6 +44,7 @@ func main() {
 
 	fmt.Printf("\nThank you. You were diagnosed with %s. Did Dr %s explain how to manage this diagnosis in a way you could understand? (yes/no)\n", diagnosis.Code.Coding[0].Name, doctor.Name[0].Family)
 
+	var understandingBool bool
 	var understandingWording string
 	understanding := ""
 	for understanding == "" {
@@ -167,8 +54,10 @@ func main() {
 		noRegex, _ := regexp.Compile("(n|no)")
 
 		if yesRegex.MatchString(strings.ToLower((understanding))) == true {
+			understandingBool = true
 			understandingWording = "do"
 		} else if noRegex.MatchString(strings.ToLower((understanding))) == true {
+			understandingBool = false
 			understandingWording = "do not"
 		} else {
 			understanding = ""
@@ -180,10 +69,16 @@ func main() {
 
 	fmt.Print("> ")
 	feeling, _ := reader.ReadString('\n')
+	feeling = strings.TrimSuffix(feeling, "\n")
 
 	fmt.Println("\n\nThanks again! Here’s what we heard:\n")
-	fmt.Printf("    You said you were %d out of 10 likely to recommend Dr %s\n", rating, doctor.Name[0].Family)
+	fmt.Printf("    You said you were %s out of 10 likely to recommend Dr %s\n", rating, doctor.Name[0].Family)
 	fmt.Printf("    You felt that you %s understand Dr %s's explanation for how to manage this diagnosis.\n", understandingWording, doctor.Name[0].Family)
 	fmt.Printf("    Your feeling about this diagnosis: %s\n", feeling)
 
+	feedback := make(map[string]string)
+	feedback["rating"] = rating
+	feedback["understanding"] = strconv.FormatBool(understandingBool)
+	feedback["feeling"] = string(feeling)
+	SaveFeedback(feedback)
 }
